ebebb8219924b16f316affab9e0c50b977a69563
Dima <dima@debian>
2014-03-26 20:56:03 +0400

    vers 2

--- filtr.c
+++ filtr.c
@@ -2,32 +2,38 @@
 #include<unistd.h>
 #include<string.h>
 #include<malloc.h>
+#include<stdlib.h>
 
 
-void filtr (char* command) {
-	FILE *f = popen(command, "r");
-	if (f == NULL) {
-		perror(command);
-		return;
-	}
+void filtr (char* argv[]) {
 	int col_str = 0;
-	char c;
-	char* buf = malloc(1);	
-	if (buf == NULL) {
-		perror(buf);
-		pclose(f);
+	int n;
+	int fd[2];
+	if (pipe(fd) < 0) {
+		perror("pipe error");
 		return;
 	}
-	c = fgetc(f);
-	while (!feof(f)) {
-		buf[0] = c;
-		write(1, buf, 1);
-		if (c == '\n')
-			col_str++;
-		c = fgetc(f);
+	if (!fork()) {
+		close(fd[0]);
+		dup2(fd[1], 1);
+		execvp(argv[1], argv + 1);
+		close(fd[1]);
+	} else {
+		close(fd[1]);
+		char* buf = malloc(1);	
+		if (buf == NULL) {
+			perror(buf);
+			close(fd[0]);
+			return;
+		}
+		while ((n = read(fd[0], buf, 1)) != 0) {
+			write(1, buf, n);
+			if (buf[0] == '\n')
+				col_str++;
+		}
+		close(fd[0]);
+		printf("Количество строк равно: %d\n", col_str);
 	}
-	pclose(f);
-	printf("Количество строк равно: %d\n", col_str);
 }
 
 
@@ -36,16 +42,6 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "Недостаточно аргументов");
 		return 1;
 	}
-	int i, reslen = 0;
-	for (i = 1; i < argc; i++) 
-		reslen += strlen(argv[i]) + 1;
-	char* arguments = malloc(reslen);
-	for (i = 1; i < argc - 1; ++i) {	
-		strcat(arguments, argv[i]);
-		strcat(arguments, " ");
-	}
-	strcat(arguments, argv[i]);
-	arguments[reslen - 1] = 0;
-	filtr(arguments);
+	filtr(argv);
 	return 0;
 }
